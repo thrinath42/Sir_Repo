e21cd5be9e401ef9aa5d6bca77e1b3fe25c7b6d9
Thrinath <Thrinath@THRINATH>
2024-11-20 13:54:57 +0530

    SCM

---
+++ AutomationFramework_12/src/test/java/keerti/Scm_Test.java
@@ -0,0 +1,12 @@
+package keerti;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Scm_Test {
+	@Test
+	public void thirst() {
+		Reporter.log("Hello",true);
+	}
+
+}

5fc8cc42e765465b91b5046685fc8235520a296a
Thrinath <Thrinath@THRINATH>
2024-11-20 13:58:46 +0530

    2nd

--- AutomationFramework_12/src/test/java/keerti/SCM1_Test.java
+++ AutomationFramework_12/src/test/java/keerti/SCM1_Test.java
@@ -3,7 +3,7 @@ package keerti;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Scm_Test {
+public class SCM1_Test {
 	@Test
 	public void thirst() {
 		Reporter.log("Hello",true);
